9af51f70f11050d244a595f57b24e8dbc5154aab
InamulShaik <139761452+InamulHussenShaik@users.noreply.github.com>
2025-09-27 16:43:54 +0530

    initial commit

---
+++ src/main/java/com/ibhg/ibha/IbhaApplication.java
@@ -0,0 +1,13 @@
+package com.ibhg.ibha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibhg/ibha/IbhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibhg.ibha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
